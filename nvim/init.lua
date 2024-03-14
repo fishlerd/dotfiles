6202836aa7c1e6abdce9c7ee66ab107583366da6
Dan Fishler <danfsh@gmail.com>
2024-03-13 20:47:00 -0600

    notify movedto bottom

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,8 @@ require("fishvim")
 require("notify").setup({
   stages = "fade_in_slide_out",
   timeout = 5000,
+  top_down = false,
+  position = "bottom_right",
   background_colour = "#000000",
   text_colour = "#dcdccc",
   icons = {
