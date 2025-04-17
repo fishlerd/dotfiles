99632951094b2d1ac760e86aeb8934c6a46e7143
Dan Fishler <danfsh@gmail.com>
2025-04-17 19:05:13 +0300

    fish add clear alias

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,14 @@ alias gg="g a .; and g c -a"
 # alias lg="lazygit"
 alias n="nvim"
 alias v="nvim"
+alias t="eza --tree"
+alias ta="eza --tree -a"
+alias l="eza -al --icons"
+alias ld="eza -ald */"
 #alias vim="nvim"
 alias z="v ~/.config/nvim/"
 alias ll="ls -alh"
+alias c="clear"
 
 # Start an SSH agent if required, if not, connect to it.
 # initialise_ssh_agent
@@ -28,4 +33,7 @@ end
 # start starship in fish
 starship init fish | source
 
-thefuck --alias | source
+# thefuck --alias | source
+
+# Created by `pipx` on 2024-07-22 06:32:22
+set PATH $PATH /Users/fish/.local/bin
